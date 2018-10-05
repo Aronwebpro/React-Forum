5e305952168785c40e7fcd1d2c6b0f3ac1bfc47d
Aronwebpro <aronwebpro@gmail.com>
2018-10-04 21:19:44 -0500

    User name in Header

--- src/components/template/navigation/Navigation.jsx
+++ src/components/template/navigation/Navigation.jsx
@@ -4,16 +4,34 @@ import {signOut} from "../../../api/auth";
 import {FlashMessageHandler} from '../../../api/FlashMessageHandler';
 
 class Navigation extends Component {
-    constructor() {
-        super();
-        this.logOut = this.logOut.bind(this);
+
+    render() {
+        const {user} = this.props;
+        return (
+            <div className="navigation-wrapper">
+                <nav
+                    className={!user || (user && !user.authorAvatar) ? 'nav-logged-out' : undefined}>
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    {user && <a className="theme-color_txt log-out" onClick={this.logOut} style={{cursor: 'pointer'}}>Logout</a>}
+                    {user && <a href="/profile" style={{marginRight: '0', fontStyle: 'italic'}} >{user.authorName}</a>}
+                    {!user &&  <a className="theme-color_txt log-out" href="/login">Login</a>}
+                    {!user &&  <a href="/register">Sign Up</a>}
+                </nav>
+                {this.props.user && this.props.user.authorAvatar && (
+                    <div className="header-avatar"><a href="/profile"><img src={this.props.user.authorAvatar} alt=""/></a>
+                    </div>)}
+            </div>
+
+        );
     }
 
     componentDidMount() {
         if (this.props.user && !this.props.user.authorAvatar) this.setState({});
     }
 
-    async logOut() {
+    //Log Out User with FireBase Handler and show message
+    logOut = async () => {
         try {
             await signOut();
             let msg = 'GoodBye! You\'ve logged out successfully!';
@@ -23,30 +41,6 @@ class Navigation extends Component {
             FlashMessageHandler.create(msg, 'error');
         }
         window.scrollTo(0, 0);
-    }
-
-    render() {
-        let log;
-        if (this.props.user) {
-            log = (<a className="theme-color_txt log-out" onClick={this.logOut} style={{cursor: 'pointer'}}>Logout</a>);
-        } else {
-            log = (<a className="theme-color_txt log-out" href="/login">Login</a>);
-        }
-        return (
-            <div className="navigation-wrapper">
-                <nav
-                    className={!this.props.user || (this.props.user && !this.props.user.authorAvatar) ? 'nav-logged-out' : undefined}>
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    {log}
-                    {!this.props.user && <a href="/register">Sign Up</a>}
-                </nav>
-                {this.props.user && this.props.user.authorAvatar && (
-                    <div className="header-avatar"><a href="/user"><img src={this.props.user.authorAvatar} alt=""/></a>
-                    </div>)}
-            </div>
-
-        );
     }
 };
 
