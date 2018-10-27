a09e613383849396ce032057698c46cb6df5ac56
Aronwebpro <aronwebpro@gmail.com>
2018-10-26 22:39:31 -0500

    Switcher component frame

---
+++ src/components/mixins/Switcher/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Switcher extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
