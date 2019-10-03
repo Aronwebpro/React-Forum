81ec1ba74598649839306942c13d3babdc3a6f82
adeimant <adeimantavicius@plaidypus.com>
2019-10-03 10:24:49 -0700

    Post and User types

---
+++ typings/index.d.ts
@@ -0,0 +1,24 @@
+declare type User = {
+    uid: string
+    authorName: string
+    authorAvatar: string
+    memberSince: number // Date
+    topics: number
+    answers: number
+}
+
+declare type Post = {
+    postId: string
+    title: string
+    text: string
+    type: string
+    category: string
+    categoryId: string
+    created: number
+    lastReply: number
+    lastUser: User
+    lastUserId: string
+    repliesCount: number
+    userId: string
+    user: User
+}
